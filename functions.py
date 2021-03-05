4ac572e0a718ed04601654f85f6dc12a36942040
Tim <readatontim@gmail.com>
2021-03-05 14:57:42 -0800

    made cutom function work

--- functions.py
+++ functions.py
@@ -13,76 +13,27 @@ class AllowValid(torch.autograd.Function):
     @staticmethod
     def forward(ctx, inputs, valid):
         '''
-        Valid is the valid indexes
+        Valid is the list of valid indexes
         '''
-        # print(ctx)
+        print(ctx)
         ctx.save_for_backward(inputs)
-        return allow_only_valid(inputs, vallid)
+        ctx.valid = valid
 
-    def allow_only_valid(self, all, valid):
-        '''
-        Returns only the valid positions as values, all else are set to 0
-        '''
-        return all.detach() * np.isin(np.arange(0,64), valid)
+        for i in range(inputs.shape[0]):
+            c = np.zeros(inputs[i].shape)
+            c[ctx.valid[i]] = 1
+            d = np.ma.masked_array(inputs[i], mask = c)
+            d *= 0
+        # print(ctx.valid[13], "<- to mask")
+        # print(inputs[13], '<- result')
 
-    def translate_to_num(location):
-        '''
-        Translate standard chess square notation to integers between 0 and 63
-        '''
-        #A1 -> 0
-        #B1 -> 1
-        add = ord(location[0]) - ord('a')
-        return (int(location[1]) - 1)*8 + add
-        # pass
+        return inputs
 
-    def translate_from_num(location):
-        '''
-        Translate integers between 0 and 63 to standard chess square notation
-        '''
-        row = location // 8 + 1
-        col = chr(location % 8 + ord('a'))
-
-        return col + str(row)
-
-
-###### needs to be put into custom file for custom pytorch autograd function
-def piece_select_loss(output, valid, target):
-    '''
-    output - output of nerual netowrk
-    valid  - possible selection of the piece in number form i.e. A1 is 0 and H8 is 63, is a list
-    target - the true piece to be selected
-    '''
-    allowed_starts = [translate_to_num(move[:2]) for move in valid]
-    possible = allow_only_valid(output, allowed_starts)
-    c = nn.CrossEntropyLoss()
-    print(possible.size())
-    return c(possible, torch.tensor([translate_to_num(target[:2])]))
-
-def legal_start(moves):
-    '''
-    Given a list of moves find the valid starting position of moves
-
-    returns an array of integers from 0-> 63
-    '''
-    legal = []
-    for move in moves:
-        legal.append(translate_to_num(move[:2]))
-
-    return legal
-
-def legal_end(moves, start):
-    '''
-    Given a list of legal moves and the starting location of the move, finds valid end positions of the move
-
-    returns an array of integers from 0->63 for leagal final piece location
-    '''
-    legal = []
-    for move in moves:
-        if moves[:2] == start:
-            legal.append(translate_to_num[-2:])
-
-    return legal
-### everything above needs to be put into a new file
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        return allow_only_valid(grad_input)
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -7,10 +7,38 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 import torch.optim as optim
+from functions import AllowValid
 
+class MaskingLayer(nn.Module):
+    def __init__(self):
+        super(MaskingLayer, self).__init__()
+
+    # def __init__(self, mask):
+    #     super(MaskingLayer, self).__init__()
+    #     self.set_mask(mask)
+
+    def forward(self, input):
+        print("doing allowvalid")
+        b = AllowValid.apply
+        a = b(input, self.to_mask)
+        print(a)
+        return AllowValid(input, self.to_mask)
+
+    def set_mask(self, mask):
+        # print('set mask to:', mask)
+        self.to_mask = mask
+
+    def has_mask(self):
+        if self.to_mask:
+            return True
+        return False
+
+# most likely should rename piece selection to a more general name as
+# it will be used twice
 class PieceSelection(nn.Module):
     def __init__(self):
         super(PieceSelection, self).__init__()
+        self.mask = MaskingLayer()
 
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
@@ -19,15 +47,72 @@ class PieceSelection(nn.Module):
             nn.Linear(512, 512),
             nn.ReLU(),
             nn.Linear(512, 64),
-            nn.ReLU()
+            self.mask
         )
 
-    def forward(self, x):
-        print(x)
+    def forward(self, x, valid = ''):
+        # print(x)
         x = self.flatten(x)
+        self.mask.set_mask(valid)
         logits = self.linear_relu_stack(x)
         return logits
 
+def translate_to_num(location):
+    '''
+    Translate standard chess square notation to integers between 0 and 63
+    '''
+    #A1 -> 0
+    #B1 -> 1
+    add = ord(location[0]) - ord('a')
+    return (int(location[1]) - 1)*8 + add
+    # pass
+
+def translate_from_num(location):
+    '''
+    Translate integers between 0 and 63 to standard chess square notation
+    '''
+    row = location // 8 + 1
+    col = chr(location % 8 + ord('a'))
+
+    return col + str(row)
+
+
+def piece_select_loss(output, valid, target):
+    '''
+    output - output of nerual netowrk
+    valid  - possible selection of the piece in number form i.e. A1 is 0 and H8 is 63, is a list
+    target - the true piece to be selected
+    '''
+    allowed_starts = [translate_to_num(move[:2]) for move in valid]
+    possible = allow_only_valid(output, allowed_starts)
+    c = nn.CrossEntropyLoss()
+    print(possible.size())
+    return c(possible, torch.tensor([translate_to_num(target[:2])]))
+
+def legal_start(moves):
+    '''
+    Given a list of moves find the valid starting position of moves
+
+    returns an array of integers from 0-> 63
+    '''
+    legal = []
+    for move in moves:
+        legal.append(translate_to_num(move[:2]))
+
+    return list(set(legal))
+
+def legal_end(moves, start):
+    '''
+    Given a list of legal moves and the starting location of the move, finds valid end positions of the move
+
+    returns an array of integers from 0->63 for leagal final piece location
+    '''
+    legal = []
+    for move in moves:
+        if moves[:2] == start:
+            legal.append(translate_to_num[-2:])
+
+    return legal
 
 if __name__ == "__main__":
 
@@ -39,15 +124,23 @@ if __name__ == "__main__":
     for i in range(len(black)):
         black[i][0] *= -1
 
+    x = [data[0] for data in black]
+    valid = [legal_start(data[1]) for data in black]
+    print(torch.tensor(x).shape)
+    print(print(len(valid)))
+
+
     test = PieceSelection()
     opt = optim.SGD(test.parameters(), lr = .01)
+    print(test)
 
     for i in range(20):
         opt.zero_grad()
-        output = test(torch.tensor([black[0][0]]).type(torch.FloatTensor))
-        loss = piece_select_loss(output, black[0][1], black[0][2])
-        loss.backward()
-        opt.step()
+        output = test(torch.tensor(x).type(torch.FloatTensor), valid)
+        # print(output)
+        # loss = piece_select_loss(x, valid, black[0][2])
+        # loss.backward()
+        # opt.step()
 
 
 
