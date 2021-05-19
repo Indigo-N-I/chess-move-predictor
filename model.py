d4da494c8750c2bf120ea40ad1697b85b5b37c9c
Tim <readatontim@gmail.com>
2021-05-18 18:41:24 -0700

    predicting type of piece moved
    still has way too much overfitting

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ from torchvision import datasets, transforms
 import torch.optim as optim
 from functions import AllowValid
 from sklearn.model_selection import train_test_split
+from collections import Counter
 
 class MaskingLayer(nn.Module):
     def __init__(self):
@@ -43,12 +44,11 @@ class PieceSelection(nn.Module):
 
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(64, 128),
-            # nn.Dropout(),
+            nn.Linear(64, 64),
+            nn.Dropout(.2),
             nn.LeakyReLU(),
-            nn.Linear(128, 64),
-            self.mask,
-            self.mask
+            nn.Dropout(.2),
+            nn.Linear(64, 6),
         )
 
     def forward(self, x, valid = ''):
@@ -58,6 +58,9 @@ class PieceSelection(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
+# try this:
+# Choose a piece (like k/r/p/q/b instead of square) then
+# predict the square to go to
 
 def translate_to_num(location):
     '''
@@ -68,6 +71,7 @@ def translate_to_num(location):
     # print(location)
     add = ord(location[0]) - ord('a')
     # print(add)
+    # print(location, (int(location[1]) - 1)*8 + add  )
     return (int(location[1]) - 1)*8 + add
     # pass
 
@@ -93,7 +97,7 @@ def piece_select_loss(output, valid, target):
     # print(possible.size())
     return c(possible, torch.tensor([translate_to_num(target[:2])]))
 
-def legal_start(moves):
+def legal_start(moves, to_set = True):
     '''
     Given a list of moves find the valid starting position of moves
 
@@ -103,7 +107,7 @@ def legal_start(moves):
     for move in moves:
         legal.append(translate_to_num(move[:2]))
 
-    return list(set(legal))
+    return list(set(legal)) if to_set else legal
 
 def legal_end(moves):
     '''
@@ -119,11 +123,24 @@ def legal_end(moves):
 
     return legal
 
+def translate_to_pieces(board, spots):
+    final_pieces = []
+    print(len(board), len(spots))
+    for i in range(len(board)):
+        # print(spots[i], spots[i]//8, spots[i]%8)
+        # print(translate_from_num(spots[i]))
+        # print(board[i])
+        a = board[i][-(spots[i]//8+1)][spots[i]%8]
+        final_pieces.append(a - 1)
+        # print(f"adding {a}")
+    return final_pieces
+
+
 if __name__ == "__main__":
 
     start = datetime(2018, 12, 8)
     end = datetime(2021, 3, 7)
-    games = 20
+    games = 300
     print("gathering games")
 
     white, black = get_moves('whoisis', start, end, games, split = True)
@@ -134,14 +151,23 @@ if __name__ == "__main__":
     # print(black[-1])
     print("transorming data")
     train, test = train_test_split(black)
+    # print(train[0])
 
     x = [data[0] for data in train]
     valid = [legal_start(data[1]) for data in train]
-    target = torch.tensor(legal_end([data[2] for data in train]))
+    # print(translate_to_pieces(train[0], train[2]))
+    # target = torch.tensor(legal_end([data[2] for data in train]))
+    counts = translate_to_pieces([data[0] for data in train], legal_start([data[2] for data in train], False))
+
+    print(Counter(counts))
+
+    target = torch.tensor(translate_to_pieces([data[0] for data in train], legal_start([data[2] for data in train], False))).type(torch.LongTensor)
 
     x_test = [data[0] for data in test]
     valid_test = [legal_start(data[1]) for data in test]
-    target_test = torch.tensor(legal_end([data[2] for data in test]))
+    # target_test = torch.tensor(legal_end([data[2] for data in test]))
+    target_test = torch.tensor(translate_to_pieces([data[0] for data in test], legal_start([data[2] for data in test], False))).type(torch.LongTensor)
+    print(target_test)
     # print(torch.tensor(x).shape)
     # print(print(len(valid)))
 
@@ -158,12 +184,15 @@ if __name__ == "__main__":
     print("training")
     for epoch in range(35):
         running_loss = 0.0
-        opt = optim.SGD(test.parameters(), lr = lr*(.9**epoch))
+        opt = optim.SGD(test.parameters(), lr = lr*(.9**epoch),weight_decay=1e-5)
         for index, data in enumerate(x):
             # print(valid[index])
             opt.zero_grad()
             output = test(torch.tensor([data]).type(torch.FloatTensor), valid[index])
             # print(torch.tensor(output).shape, output)
+            # print(target.shape, output.shape)
+            # print(target, index)
+            # print(output, torch.tensor([target[index]]))
             loss = criterion(output, torch.tensor([target[index]]))
             loss.backward()
             opt.step()
@@ -173,16 +202,20 @@ if __name__ == "__main__":
                 print('[%d, %5d] loss: %.3f' %
                       (epoch + 1, index + 1, running_loss / 2000))
                 running_loss = 0.0
-        print(f"epoch {epoch} results")
+        # print(f"epoch {epoch} results, index max: {index}")
         correct = 0
         quest = 0
+        test_loss = 0
         for index, data in enumerate(x_test):
             output = test(torch.tensor([data]).type(torch.FloatTensor), valid_test[index])
+            loss = criterion(output, torch.tensor([target_test[index]]))
+            test_loss += loss.item()
             _, predicted = torch.max(output, 1)
             if predicted[0] == target_test[index]:
                 correct += 1
             quest += 1
         print(f"Correct Precent test: {correct/quest}")
+        print(f"Test loss: {test_loss/index}")
 
     correct = 0
     quest = 0
--- moves.py
+++ moves.py
@@ -8,7 +8,7 @@ import io
 import numpy as np
 from get_games import get_games, split_bw
 
-def process_board(iBoard):
+def process_board(iBoard, not_dummy = True):
     '''
     pawn = 1
     knight = 2
@@ -25,17 +25,17 @@ def process_board(iBoard):
     piece_translation = {
         '.': 0,
         'p': -1,
-        'n': -2,
-        'b': -3,
-        'r': -4,
-        'q': -5,
-        'k': -6,
-        'P': 1,
-        'N': 2,
-        'B': 3,
-        'R': 4,
-        'Q': 5,
-        'K': 6
+        'n': -2 if not_dummy else -1,
+        'b': -3 if not_dummy else -1,
+        'r': -4 if not_dummy else -1,
+        'q': -5 if not_dummy else -1,
+        'k': -6 if not_dummy else -1,
+        'P': 1 if not_dummy else 1,
+        'N': 2 if not_dummy else 1,
+        'B': 3 if not_dummy else 1,
+        'R': 4 if not_dummy else 1,
+        'Q': 5 if not_dummy else 1,
+        'K': 6 if not_dummy else 1
     }
     for index, line in enumerate(board):
         np_board.append([])
@@ -43,13 +43,13 @@ def process_board(iBoard):
             if char in piece_translation:
                 np_board[index].append(piece_translation[char])
             # print(np_board)
-
+    # print(iBoard.legal_moves)
     np_board = np.array(np_board)
     # print(np_board)
     # print(iBoard)
     return np_board
 
-def gen_data(game, white: bool):
+def gen_data(game, white: bool, not_dummy = True):
     pgn = io.StringIO(game)
     game = chess.pgn.read_game(pgn)
     board = game.board()
@@ -59,43 +59,43 @@ def gen_data(game, white: bool):
     save_move = white
     for move in game.mainline_moves():
         # print(move)
-        # print(board)
+        # print(board.legal_moves)
         if save_move:
             legal = []
             for legal_move in board.legal_moves:
                 legal.append(str(legal_move))
 
-            data.append([process_board(str(board)), legal, str(move)])
+            data.append([process_board(str(board), not_dummy), legal, str(move)])
 
         board.push(move)
         save_move = not save_move
 
     return data
 
-def gen_all(white_games, black_games):
+def gen_all(white_games, black_games, not_dummy = True):
     data = []
-    data.extend(gen_games(white_games, True))
-    data.extend(gen_games(black_games, False))
+    data.extend(gen_games(white_games, True, not_dummy))
+    data.extend(gen_games(black_games, False, not_dummy))
 
     return data
 
-def gen_games(games, white):
+def gen_games(games, white, not_dummy = True):
     data = []
 
     for game in games:
-        data.extend(gen_data(game, white))
+        data.extend(gen_data(game, white, not_dummy))
 
     return data
 
-def get_moves(name, start, end, games, split = True):
+def get_moves(name, start, end, games, split = True, not_dummy = True):
     retreaved_games = get_games('whoisis', start, end, games)
 
     white, black = split_bw(retreaved_games, 'whoisis')
 
     if split:
-        return gen_games(white, True), gen_games(black, False)
+        return gen_games(white, True, not_dummy), gen_games(black, False, not_dummy)
 
-    data = gen_all(white, black)
+    data = gen_all(white, black, not_dummy)
 
     return data
 
